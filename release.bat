d124ba40a63086b41c9aa2585aced8b5c5488774
Macc Liu <maccliu@qq.com>
2017-11-14 15:31:01 +0800

    完成后，等待15秒，自行退出

--- release.bat
+++ release.bat
@@ -6,6 +6,7 @@ copy /y  composer.json  "%TARGET_DIR%\"
 copy /y  .gitignore     "%TARGET_DIR%\"
 
 del /f /s /q            "%TARGET_DIR%\src\*.*"
+rd /s /q                "%TARGET_DIR%\src\"
 xcopy /y /s  src        "%TARGET_DIR%\src\"
 
-pause
+ping -n 15 127.0.0.1>nul
